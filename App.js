0c86f68ba02cbc37990b8438280c0a8a78a37c95
PauloPZ <pc.zuit@gmail.com>
2024-02-21 15:30:26 -0300

    Só pra ter certeza né

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+ 
   return (
     <View style={styles.container}>
       <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
       </View>
       <View style={styles.secondaryContainer}>
         <Text style={{ color: 'blue', fontSize: 30, fontWeight: 'bold', textAlign: 'end' }}>Paulo Cesar</Text>
-        <Text style={{ color: 'blue', fontSize: 25, fontStyle: 'italic', textAlign: 'start', borderWidth: 1, borderColor: 'black', borderRadius: 15 }}>30</Text>
-        <Text style={{ color: 'black', backgroundColor: 'pink', fontSize: 20, fontStyle: 'italic', textAlign: 'center' }}>Sala 7</Text>
+        <TextInput style={styles.inputName} ></TextInput>
+        <TextInput style={styles.inputName} placeholder='Digite seu nome' secureTextEntry= {true} keyboardType='numeric' ></TextInput>
+        <Text style={{ color: 'blue', fontSize: 25, fontStyle: 'italic', textAlign: 'start', borderWidth: 1, borderColor: 'black', borderRadius: 15, marginTop: 15 }}>30</Text>
+        <Text style={{ color: 'black', backgroundColor: 'pink', fontSize: 20, fontStyle: 'italic', textAlign: 'center', marginTop: 15 }}>Sala 7</Text>
+        <Text style={{color: 'black',fontSize: 25, backgroundColor: 'gray', borderWidth: 1, borderColor: 'black', borderRadius: 15, textAlign: 'center', marginTop: 15}}>Kuroi Killa</Text>
       </View>
       <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
       </View>
@@ -27,4 +31,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#fff',
   },
+  inputName: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 15,
+    fontSize: 20,
+    placeholder:'Digite seu nome',
+    inputMode: 'text',
+    placeholderTextColor: 'red'
+  }
 });
