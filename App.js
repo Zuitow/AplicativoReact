49baa8052b5087efac41645bcce2c0a636f3085a
PauloPZ <pc.zuit@gmail.com>
2024-02-21 12:13:28 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -4,8 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+      </View>
+      <View style={styles.secondaryContainer}>
+        <Text style={{ color: 'blue', fontSize: 30, fontWeight: 'bold', textAlign: 'end' }}>Paulo Cesar</Text>
+        <Text style={{ color: 'blue', fontSize: 25, fontStyle: 'italic', textAlign: 'start', borderWidth: 1, borderColor: 'black', borderRadius: 15 }}>30</Text>
+        <Text style={{ color: 'black', backgroundColor: 'pink', fontSize: 20, fontStyle: 'italic', textAlign: 'center' }}>Sala 7</Text>
+      </View>
+      <View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  secondaryContainer: {
+    flex: 1,
+    width: '100%',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
 });
